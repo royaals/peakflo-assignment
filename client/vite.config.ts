a65cf7b6e8f8c9d6bda6bc2171f5f53fafdd1ffa
Royal <royal.21ai040@sode-edu.in>
2025-01-25 13:32:34 +0530

    Add Tailwind css and shadcn

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
+import { loadEnv } from 'vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    define: {
+      'process.env': env
+    },
+  }
 })
